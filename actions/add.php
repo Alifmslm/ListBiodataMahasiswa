8df73a97337f1ee233b639b93cc4c484759e149f
Alifmslm <alifmslm01@gmail.com>
2025-09-13 21:15:30 +0700

    ADD: Action Files & Re-structure File and Folder

---
+++ actions/add.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    # code...
+    $listMahasiswa[] = [
+        "nama" => $_POST["name_input"],
+        "umur" => $_POST["age_input"],
+        "jurusan" => $_POST["jurusan_input"],
+        "hobby" => $_POST["hobby_input"],
+    ];
+
+    $_SESSION['mahasiswa'] = $listMahasiswa;
+
+    header('Location: index.php');
+    exit();
+}
+?>
--- form.php
+++ form.php
@@ -1,19 +1,7 @@
 <?php
     session_start();
     include("data.php");
-
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        # code...
-        $listMahasiswa[] =[
-            "nama" => $_POST["name_input"],
-            "umur" => $_POST["age_input"],
-            "jurusan" => $_POST["jurusan_input"],
-            "hobby" => $_POST["hobby_input"],
-        ];
-
-        $_SESSION['mahasiswa'] = $listMahasiswa;
-
-        header('Location: index.php');
-        exit();
-    }
+    include("./actions/add.php");
+    include("./actions/delete.php");
+    include("./actions/update.php");
 ?>
