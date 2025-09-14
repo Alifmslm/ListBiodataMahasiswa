72eb47b838600d5e14d9b724e30f8f2569f2c262
Alifmslm <alifmslm01@gmail.com>
2025-09-14 19:52:40 +0700

    ADD: ID In ListMahasiswa

--- actions/add.php
+++ actions/add.php
@@ -1,7 +1,10 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     # code...
+    $countMahasiswa = count($listMahasiswa);
+
     $listMahasiswa[] = [
+        "id" => $countMahasiswa + 2,
         "nama" => $_POST["name_input"],
         "umur" => $_POST["age_input"],
         "jurusan" => $_POST["jurusan_input"],
--- data.php
+++ data.php
@@ -3,6 +3,7 @@
 if (!isset($_SESSION['mahasiswa'])) {
     $_SESSION['mahasiswa'] = [
         [
+            "id" => 1,
             "nama" => "Gattuzo",
             "umur" => 19,
             "jurusan" => "Sistem Informasi",
